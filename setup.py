cb72a77bd9ce4f017508bffdce9d37ce1f273a0a
Andrew Lorente <hello@andrewlorente.com>
2015-04-22 10:20:51 -0500

    Basic skeleton of the script

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+
+setup(name="gitignore-fetch",
+      version="1.0.0",
+      description="Fetch the .gitignore defined by GitHub for a given language",
+      author="Andrew Lorente",
+      author_email="hello@andrewlorente.com",
+      url="",
+      packages=[],
+      scripts=['gitignore-fetch'],
+      install_requires=[
+          "requests>=2.6.0",
+      ],
+      )
